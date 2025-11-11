53fa655f55884d00a1ca0068cf3aaf35dcba3c01
GagTad <136594120+GagTad@users.noreply.github.com>
2025-11-12 01:12:08 +0400

    Update main.cpp

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,18 +1,12 @@
-#include "mainwindow.h" // Ներառում ենք մեր գլխավոր պատուհանի header-ը
+#include "mainwindow.h" 
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    // 1. Ստեղծում ենք գրաֆիկական հավելվածի օբյեկտը
     QApplication a(argc, argv);
-
-    // 2. Ստեղծում ենք մեր գլխավոր պատուհանի օրինակը
     MainWindow w;
-
-    // 3. Ցուցադրում ենք պատուհանը էկրանին
     w.show();
-
-    // 4. Գործարկում ենք հավելվածը և սպասում, մինչև օգտատերը կփակի այն
     return a.exec();
 }
+
